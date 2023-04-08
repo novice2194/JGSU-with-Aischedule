80b3a34d15791667b963681728c8f10cf76faaa5
novice2194 <2194150786@qq.com>
2023-04-08 21:06:00 +0800

    modified:   scheduleHtmlParser.js

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -1,8 +1,27 @@
 /*
  * @Author: BeYoung
  * @Date: 2023-04-07 18:59:58
- * @LastEditTime: 2023-04-08 18:11:39
+ * @LastEditTime: 2023-04-08 19:59:01
  */
+
+class Course {
+    day = 0;
+    name = "name";
+    weeks = [];
+    teacher = "teacher";
+    position = "position";
+    sections = [];
+    constructor(name, teacher, position, day, weeks, sections) {
+        this.day = day;
+        this.name = name;
+        this.weeks = weeks;
+        this.teacher = teacher;
+        this.position = position;
+        this.sections = sections;
+    }
+}
+
+
 function scheduleHtmlParser(html) {
     let result = [];
     const schedule = JSON.parse(html);
@@ -10,9 +29,10 @@ function scheduleHtmlParser(html) {
         console.log(schedule)
         throw new Error("获取课表失败");
     }
+    for (const courses of schedule.data) {
+        for (const couse of courses) {
 
-    for (course in schedule.data) {
-        
+        }
     }
     console.log(result);
     return result;
