cbff430c5bc6d589febd4619e4624969b03ccc34
novice2194 <2194150786@qq.com>
2023-04-08 18:07:33 +0800

    fix: scheduleHtmlProvider.js

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,24 +1,59 @@
-function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {//函数名不要动
-    // 以下可编辑
-    const ifrs = dom.getElementsByTagName("iframe");
-    const frs = dom.getElementsByTagName("frame");
-    if (ifrs.length) {
-        for (let i = 0; i < ifrs.length; i++) {
-            const dom = ifrs[i].contentDocument.body.parentElement;
-            iframeContent += getPageHtml(iframeContent, frameContent, dom);
-        }
+/*
+ * @Author: BeYoung
+ * @Date: 2023-04-07 18:59:58
+ * @LastEditTime: 2023-04-08 18:01:37
+ */
+
+function decodeCookie(cookie) {
+    let cookieObj = {};
+    let cookieArr = cookie.split("; ");
+    for (var i = 0; i < cookieArr.length; i++) {
+        var arr = cookieArr[i].split("=");
+        if (arr[0] != "user") continue;
+        cookieObj[arr[0]] = JSON.parse(arr[1]);
     }
-    if (frs.length) {
-        for (let i = 0; i < frs.length; i++) {
-            const dom = frs[i].contentDocument.body.parentElement;
-            frameContent += getPageHtml(iframeContent, frameContent, dom);
-        }
+    return cookieObj;
+}
+
+async function getWeeks(schoolYear) {
+    const url = "/api/arrange/teacherServer/queryWeek?schoolYear=" + schoolYear;
+    let weeks = await fetch(url, {
+        method: 'get',
+        redirect: 'follow',
+        "headers": {
+            "content-type": "application/json;charset=UTF-8",
+        },
+    });
+    return weeks.json();
+}
+
+async function getSchedule(cookie, weeks) {
+    const now = Date.now().toString().substring(0, 10);
+    const url = "/api/arrange/CourseScheduleAllQuery/studentCourseSchedule?_t=" + now;
+    let schedule = await fetch(url, {
+        method: 'POST',
+        redirect: 'follow',
+        "headers": {
+            "content-type": "application/json;charset=UTF-8",
+        },
+        body: JSON.stringify({
+            studentID: cookie.user.studentID,
+            semester: cookie.user.semester,
+            oddOrDouble: 0,
+            weeks: weeks
+        })
+    });
+    return schedule.json();
+}
+
+async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) { //函数名不要动
+    const cookie = decodeCookie(decodeURI(document.cookie));
+    const weeks = await getWeeks(cookie.user.schoolYear);
+    if (weeks.code >= 400) {
+        console.log(weeks)
+        throw new Error("获取周次失败");
     }
-    if (!ifrs.length && !frs.length) {
-        return dom.querySelector('body').outerHTML
-    }
-    if (dom === document) {
-        return document.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent
-    }
-    return iframeContent + frameContent;
+    const schedule = await getSchedule(cookie, weeks.data);
+    console.log(schedule);
+    return JSON.stringify(schedule);
 }
