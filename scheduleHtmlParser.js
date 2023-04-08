1cd465960339002f81a08bb18dc568cc2861ea63
novice2194 <2194150786@qq.com>
2023-04-08 19:42:27 +0800

    modified:   scheduleHtmlParser.js modified:   scheduleHtmlProvider.js

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -1,102 +1,19 @@
+/*
+ * @Author: BeYoung
+ * @Date: 2023-04-07 18:59:58
+ * @LastEditTime: 2023-04-08 18:11:39
+ */
 function scheduleHtmlParser(html) {
     let result = [];
-    let parserText = $('tbody')[0];
-    let parserTextChildrenLength = parserText.children.length;
-    let flagOfDoubleSections = true;
-    console.log(parserText);
-    let text = parserText[0];
-    console.log(text)
-
-    if (parserTextChildrenLength >= 11) flagOfDoubleSections = false;
-
-    for (let i = 0; i < parserTextChildrenLength; i++) {
-        for (let j = 1; j < 8; j++) {
-            //空课程
-            if (parserText.children[i].children[j].children[0].children.length == 0) continue;
-
-            //课程解析文本
-            let classParserText = parserText.children[i].children[j].children[0].children;
-            //console.log(classParserText);
-
-            //解析课程信息
-            for (let k = 0; k < classParserText.length; k++) {
-                //提取课程信息
-                let classText = classParserText[k];
-                let name = classText.children[0].children[0].data;
-                let time = classText.children[1].children[0].children[0].children[0].data;
-                let section = classText.children[1].children[0].children[1].children[0].data;
-                let position = classText.children[3].children[0].children[0].children[0].data;
-                let teacher = classText.children[4].children[0].children[0].children[0].data;
-
-                //处理起始周数
-                console.log(time);
-                let weeksOfStart = '';
-                let weeksOfEnd = '';
-                for (let l = 0; l < time.length; l++) { //开始周
-                    if (time[l] == ' ' || time[l] == '-') break;
-                    weeksOfStart += time[l];
-                }
-                for (let l = time.search('-') + 1; l != 0 && l < time.length; l++) { //结束周
-                    if (time[l] == ' ') break;
-                    weeksOfEnd += time[l];
-                }
-                if (weeksOfEnd.length == 0) weeksOfEnd = weeksOfStart;
-                weeksOfStart = Number(weeksOfStart);
-                weeksOfEnd = Number(weeksOfEnd);
-
-                //处理单双周
-                let flagOfWeeks = false; //是否为单双周课程
-                let weeks = []; //添加周数
-                if (time.search('单') > 0 || time.search('双') > 0) flagOfWeeks = true;
-                if (!flagOfWeeks) {
-                    for (let w = weeksOfStart; w <= weeksOfEnd; w++) weeks.push(w);
-                } else {
-                    for (let w = weeksOfStart; w <= weeksOfEnd; w += 2) weeks.push(w);
-                }
-
-                //处理节数
-                console.log(section);
-                let sections = [];
-                if (!flagOfDoubleSections) {
-                    sections.push(i + 1);
-                } else {
-                    let sectionOfOne = ''; //第一节课
-                    let sectionOfTwo = ''; //第二节课
-
-                    if (section.search('第') != -1) {
-                        for (let l = section.search('第') + 1; section[l] != ',' && section[l] != '节'; l++) {
-                            sectionOfOne += section[l];
-                        }
-                    }
-                    if (section.search(',') != -1) {
-                        for (let l = section.search(',') + 1; section[l] != '节'; l++) {
-                            sectionOfTwo += section[l];
-                        }
-                    }
-
-                    if (sectionOfOne.length > 0) sections.push(Number(sectionOfOne)); //判断是否有课程
-                    if (sectionOfTwo.length > 0) sections.push(Number(sectionOfTwo)); //有第二节课时才添加到课程中
-                }
-
-                //添加课程信息
-                let re = {sections: [], weeks: []}
-                re.name = name;
-                re.position = position;
-                re.teacher = teacher;
-                re.day = j.toString();
-                re.sections = sections;
-                re.weeks = weeks;
-
-                if (re.weeks.length == 0) {
-                    continue;
-                }
-                //console.log(re);
-                result.push(re);
-            }
-        }
+    const schedule = JSON.parse(html);
+    if (schedule.code >= 400) {
+        console.log(schedule)
+        throw new Error("获取课表失败");
     }
 
-
+    for (course in schedule.data) {
+        
+    }
     console.log(result);
     return result;
 }
--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,7 +1,7 @@
 /*
  * @Author: BeYoung
  * @Date: 2023-04-07 18:59:58
- * @LastEditTime: 2023-04-08 18:01:37
+ * @LastEditTime: 2023-04-08 19:39:32
  */
 
 function decodeCookie(cookie) {
@@ -15,45 +15,27 @@ function decodeCookie(cookie) {
     return cookieObj;
 }
 
-async function getWeeks(schoolYear) {
-    const url = "/api/arrange/teacherServer/queryWeek?schoolYear=" + schoolYear;
-    let weeks = await fetch(url, {
-        method: 'get',
-        redirect: 'follow',
-        "headers": {
-            "content-type": "application/json;charset=UTF-8",
-        },
-    });
-    return weeks.json();
-}
-
 async function getSchedule(cookie, weeks) {
-    const now = Date.now().toString().substring(0, 10);
-    const url = "/api/arrange/CourseScheduleAllQuery/studentCourseSchedule?_t=" + now;
-    let schedule = await fetch(url, {
-        method: 'POST',
-        redirect: 'follow',
+    const url = "/api/arrange/CourseScheduleAllQuery/studentCourseSchedule";
+    const schedule = await fetch(url, {
         "headers": {
             "content-type": "application/json;charset=UTF-8",
         },
-        body: JSON.stringify({
-            studentID: cookie.user.studentID,
-            semester: cookie.user.semester,
-            oddOrDouble: 0,
-            weeks: weeks
-        })
+        "body": JSON.stringify({
+            "studentId": cookie.user.userName,
+            "semester": cookie.user.semester,
+            "weeks": weeks,
+            "oddOrDouble": 0,
+        }),
+        "method": "POST",
     });
     return schedule.json();
 }
 
 async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) { //函数名不要动
     const cookie = decodeCookie(decodeURI(document.cookie));
-    const weeks = await getWeeks(cookie.user.schoolYear);
-    if (weeks.code >= 400) {
-        console.log(weeks)
-        throw new Error("获取周次失败");
-    }
-    const schedule = await getSchedule(cookie, weeks.data);
+    const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const schedule = await getSchedule(cookie, weeks);
     console.log(schedule);
     return JSON.stringify(schedule);
 }
